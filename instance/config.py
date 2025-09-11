319d2ba0feb3875c3c21d29c27a6a68d382f6fb2
eric861129 <iamhandsomeboy1129@gmail.com>
2025-09-11 17:26:29 +0800

    Fix: Correct flake8 errors in instance/config.py

--- instance/config.py
+++ instance/config.py
@@ -1,6 +1,11 @@
 import os
 
+# Get the absolute path of the directory where the file is located
+basedir = os.path.abspath(os.path.dirname(__file__))
+
+
 class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'a-secret-key'
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'sqlite:///' + os.path.join(os.path.abspath(os.path.dirname(__file__)), 'app.db')
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or \
+        'sqlite:///' + os.path.join(basedir, 'app.db')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
